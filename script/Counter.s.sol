50819f8293ce98a3906496434e8880b7c4e6a4c5
gcranju <qa5505815@gmail.com>
2023-12-01 15:31:20 +0545

    foundry upgradeable added

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,18 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+
+import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
+        address proxy = Upgrades.deployUUPSProxy(
+            "Counter.sol",
+            abi.encodeCall(Counter.initialize, 1)
+        );
     }
 }
--- src/Counter.sol
+++ src/Counter.sol
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import "openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
 
-contract Counter {
+contract Counter is Initializable {
     uint256 public number;
+
+    function initialize(uint256 _number) public initializer {
+        number = _number;
+    }
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
